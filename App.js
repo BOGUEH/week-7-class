e2c717a7cf53c5cddf1ba4a3639c5556935b414d
BOGUEH <85343204+BOGUEH@users.noreply.github.com>
2021-07-24 11:03:03 +0100

    creating a login logout

--- App.js
+++ App.js
@@ -10,18 +10,27 @@ import {useState} from 'react';
 
 function App() {
 
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
+  const [isLog, setIslog] = useState(false);
 
   const handleClick = ()=>{
-      setCount(count +5);
-
-  };
+      setCount(count *78900999);
+      };
  
-
+    const changeLog = () =>{
+      setIslog(!isLog);
+    };
   return (
     <div className="App">
+{/* creating a login and logout using a tenary operation */}
+{/* using tenary operation */}
+        {isLog ? <p>We are logged in</p>: <p>You have been logged out, try to log in</p>} 
+      <button onClick= {changeLog}>
+      {isLog ? <span>logout</span>: <span>login</span>} 
+        </button>
 
         <Product count= {count}  handleClick= {handleClick} />
+
         {/* <First /> */}
 
     
